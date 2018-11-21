eb630f5254227c1e4cbecfd156b3afffff521cf0
DANDARA MARIA COSTA DE SOUSA 114110434 <dandaramcs@lcc3-083.lcc.ufcg.edu.br>
2018-11-21 17:28:43 -0300

    login usuario

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react';
 import Aluno from '../aluno/Aluno';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import './Login.css';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          email: "",
+          password: ""
+        };
+      }
+      validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+      }
+    
+      handleChange = event => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+      }
+    
+      handleSubmit = event => {
+        event.preventDefault();
+      }
     render() {
         return (
-            <div className="container">
+            <div className="container login">
                 <div className="box">
                 Matricula:
                 Senha:
+                
                 <Aluno/>
 
                 </div>
+
+                <div className="login">
+                    <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="email" bsSize="large">
+                        <ControlLabel>Email</ControlLabel>
+                        <FormControl
+                        autoFocus
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bsSize="large">
+                        <ControlLabel>Password</ControlLabel>
+                        <FormControl
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        bsSize="large"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                    </form>
+      </div>
 
             </div>
         );
